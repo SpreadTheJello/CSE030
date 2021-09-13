310d830338cd1d992dc69517f2890851bdc612ac
Jello F <mrjello@Jellos-iMac.local>
2021-09-13 10:23:18 -0700

    array2 finished

--- Lab_02/array2.cpp
+++ Lab_02/array2.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+string str = "";
 
 int main(){
+    cout << "Enter the string to reverse: ";
+    cin >> str;
+    for(int i = 0; i <= str.length(); i++){
+        cout << str[str.length() - i];
+    }
+    cout << endl;
     return 0;
 }
