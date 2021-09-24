071d37bc4af4ced33218bce01482f8df52443e30
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2021-09-24 12:23:24 -0700

    Lab 03 - Complete

--- Lab_03/sortArray1.cpp
+++ Lab_03/sortArray1.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
 
+using namespace std;
+int arr_size;
+
+void sort(int arr[]){
+    cout << "This is the sorted array in descending order: ";
+    for(int i = 0; i < arr_size; i++){
+        int indx = 0;
+        int max = arr[i];
+        for(int j = i; j < arr_size - 1; j++){
+            if(max <= arr[j + 1]){
+                max = arr[j + 1];
+                indx = j + 1;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = max;
+        arr[indx] = temp;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << "The algorithm selected the maximum for the traverse of the array." << endl;
+}
+
+
 int main(){
-    return 0;
+    cout << "Enter the size of the array: ";
+    cin >> arr_size;
+
+    if(arr_size < 0){ // error handling
+        cout << "ERROR: you entered an incorrect value for the array size!" << endl;
+        return 0;
+    }
+
+    // gets array from user
+    cout << "Enter the numbers in the array, seperated by a space, and press enter: ";
+    int arr[arr_size];
+    for(int i = 0; i < arr_size; i++){
+        scanf("%d", &arr[i]); // accepts integers (+ -) and inputs them into array
+    }
+    sort(arr);
+    return 0;  
 }
--- Lab_03/sortArray2.cpp
+++ Lab_03/sortArray2.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
 
+using namespace std;
+int arr_size;
+
+void sort(int arr[]){
+    int swap = 0;
+    cout << "This is the sorted array in ascending order: ";
+    for(int i = 0; i < arr_size; i++){
+        int indx = 0;
+        int min = arr[i];
+        for(int j = i + 1; j < arr_size - 1; j++){
+            if(min >= arr[j + 1]){
+                min = arr[j + 1];
+                indx = j + 1;
+                swap++;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = min;
+        arr[indx] = temp;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << "The algorithm selected the minimum for the traverse of the array." << endl;
+    cout << "It took " << swap << " swaps to sort the array..." << endl;
+}
+
+
 int main(){
-    return 0;
+    cout << "Enter the size of the array: ";
+    cin >> arr_size;
+
+    if(arr_size < 0){ // error handling
+        cout << "ERROR: you entered an incorrect value for the array size!" << endl;
+        return 0;
+    }
+
+    // gets array from user
+    cout << "Enter the numbers in the array, seperated by a space, and press enter: ";
+    int arr[arr_size];
+    for(int i = 0; i < arr_size; i++){
+        scanf("%d", &arr[i]); // accepts integers (+ -) and inputs them into array
+    }
+    sort(arr);
+    return 0;  
 }
