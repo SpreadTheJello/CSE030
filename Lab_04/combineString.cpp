cce94ec6a2e336911980bbc4753d5773c5d665bf
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2021-09-29 16:42:14 -0700

    combineString - Complete

--- Lab_04/combineString.cpp
+++ Lab_04/combineString.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string str;
+int num = 1;
+
+void combineStr(string str, int num){
+    for(int i = 0; i < num; i++){
+        cout << str;
+    }
+    cout << endl;
+}
+
 int main(){
+    while(num != 0){
+        cout << "Enter a string: ";
+        cin >> str;
+        cout << "Enter a number of times: ";
+        cin >> num;
+        combineStr(str, num);
+    }
     return 0;
 }
