d1d0a4bd59cc22d3130b5ab0107bdcd5ec7506c8
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2021-10-05 11:00:19 -0700

    first file ready

---
+++ Lab_05/fileIO.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    string *words;
+    int count;
+
+    ofstream file;
+    file.open ("words_in.txt");
+    file.close();
+    return 0;
+}
